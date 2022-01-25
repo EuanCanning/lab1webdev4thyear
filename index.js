30ff7ce7538dbadd3691aaa86758ab7ccc2c6204
EUAN CANNING (1812115) <e.canning@rgu.ac.uk>
2022-01-25 16:23:15 +0000

    js testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require("http");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 http.createServer(function(request,response){
 	response.writeHead(200,{'Content-Type':'text/plain'});
 	response.end('Hello World\n');
