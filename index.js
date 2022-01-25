4272b1553330c6c2590f19b8c8f1b7a0ec371359
EUAN CANNING (1812115) <e.canning@rgu.ac.uk>
2022-01-25 16:24:03 +0000

    js testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require("http");
 const PORT = process.env.PORT || 8080;
 http.createServer(function(request,response){
 	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end('Hello World\n');
+	response.end('Hello World #noscope360\n');
 }).listen(PORT);
 
 console.log('Sever running at $ { PORT }');
