997db2f7226dc789da999d76c50f36b76b2fb5d3
EUAN CANNING (1812115) <e.canning@rgu.ac.uk>
2022-01-25 15:59:28 +0000

    added index

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http')
 const fs = require('fs')
+
+const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
  res.writeHead(200, { 'content-type': 'text/html' })
- const PORT = process.env.PORT || 3000;
 
  
  fs.createReadStream('4thyeartest').pipe(res)
